1145b3fff12af570a8696ce78e56bfddd0d8fd6d
noisyfox <timemanager.rick@gmail.com>
2019-05-21 23:00:15 +1000

    Use manual entry point

---
+++ Service/App.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace Service
+{
+    class App: Application
+    {
+        /// <summary>
+        /// InitializeComponent
+        /// </summary>
+        private void InitializeComponent()
+        {
+            StartupUri = new Uri("MainWindow.xaml", System.UriKind.Relative);
+        }
+
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
